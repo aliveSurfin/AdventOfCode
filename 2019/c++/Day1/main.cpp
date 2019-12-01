a0b93f4dcaa1ad5296c6cec411b08aaa985fafff
alivesurfin <alivesurfin@gmail.com>
2019-12-01 18:19:00 +0000

    timing

--- 2019/c++/Day1/main.cpp
+++ 2019/c++/Day1/main.cpp
@@ -1,21 +1,35 @@
 #include "../utils.h"
 #include <stdio.h>
 #include <math.h>
-#include <iomanip> 
+#include <iomanip>
+#include <chrono>
 int main()
 {
     vector<string> input;
     Utils utils;
     input = utils.loadFile("input.txt");
     double total;
+    auto start = chrono::high_resolution_clock::now();
     for (unsigned x = 0; x < input.size(); x++)
     {
-        double temp = stod(input.at(x));
-        temp /= 3;
-        temp = floor(temp);
-        temp -= 2;
-        total += temp;
+        double test = stod(input.at(x));
+        while (test > 0)
+        {
+            test /= 3;
+            test = floor(test);
+            test -= 2;
+            if (test > 0)
+            {
+                total += test;
+            }
+        }
     }
-    cout << setprecision(15) << total << endl; // 3477353
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+
+    // To get the value of duration use the count()
+    // member function on the duration object
+    cout << duration.count() << endl;
+    cout << setprecision(15) << total << endl; // 3477353 // 5213146
     return 0;
 }
