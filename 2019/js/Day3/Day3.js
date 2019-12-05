57c47d6f2eac65e36db3be198fb5aa6449ad943b
AliveSurfin <alivesurfin@gmail.com>
2019-12-05 02:27:56 +0000

    day3 js has edge case bugs

--- 2019/js/Day3/Day3.js
+++ 2019/js/Day3/Day3.js
@@ -45,6 +45,51 @@ function intersect(v1, v2) {
     // return r, 0, errors.New("no intersection")
     return "NO";
 }
+function det(a, b, c, d) {
+    return (a * d) - (b * c);
+}
+function intersect2(v1, v2) {
+    var x1 = v1.p1.x;
+    var y1 = v1.p1.y;
+    var x2 = v1.p2.x;
+    var y2 = v1.p2.y;
+    var x3 = v2.p1.x;
+    var y3 = v2.p1.y;
+    var x4 = v2.p2.x;
+    var y4 = v2.p2.y;
+
+    var detl1 = det(x1, y1, x2, y2);
+    var detl2 = det(x3, y3, x4, y4);
+
+    var x1mx2 = x1 - x2;
+    var x3mx4 = x3 - x4;
+
+    var y1my2 = y1 - y2;
+    var y3my4 = y3 - y4;
+
+    var xnom = det(detl1, x1mx2, detl2, x3mx4);
+    var ynom = det(detl1, y1my2, detl2, y3my4);
+    var denom = det(x1mx2, y1my2, x3mx4, y3my4);
+
+    if (denom == 0) {
+        // console.log("NO");
+
+        return "NO";
+    }
+    var xout = xnom / denom;
+    var yout = ynom / denom;
+    if (!isFinite(xout) || !isFinite(yout)) {
+        return "NO";
+    }
+    var p = new point(xout, yout);
+    return p;
+
+
+
+}
+function insersect3(v1, v2) {
+
+}
 function between(p, a, b) {
     return ((p <= b && p >= a) || (p >= b && p <= a));
 }
@@ -52,12 +97,12 @@ function distance(p1, p2) {
     return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
 }
 
-function calculateAndDisplay(commands1, commands2) {
+function calculateAndDisplay(input1, input2) {
 
-    commands1 = commands1.split(",");
-    commands2 = commands2.split(",");
-    commands1.unshift("R0");
-    commands2.unshift("R0");
+    commands1 = input1.split(",");
+    commands2 = input2.split(",");
+    // commands1.unshift("R0");
+    // commands2.unshift("R0");
     var canvas = document.getElementById("myCanvas");
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
@@ -78,6 +123,7 @@ function calculateAndDisplay(commands1, commands2) {
     var points2 = new Array();
     curx = 0;
     cury = 0
+    points1.push(new point(0, 0));
     for (let a = 0; a < commands1.length; a++) { // parse commands 1
         var curvalue = parseInt(commands1[a].substr(1));
 
@@ -101,6 +147,7 @@ function calculateAndDisplay(commands1, commands2) {
     }
     curx = 0;
     cury = 0
+    points2.push(new point(0, 0));
     for (let a = 0; a < commands2.length; a++) { // parse commands 2
         var curvalue = parseInt(commands2[a].substr(1));
         if (commands2[a][0] == 'L') {
@@ -121,8 +168,9 @@ function calculateAndDisplay(commands1, commands2) {
             cury = y;
         }
     }
-    for (let a = 0; a < points1.length; a++) { // get max of points1
-        if (commands1[a][0] == 'L' || commands1[a][0] == 'R') {
+
+    for (let a = 1; a < points1.length; a++) { // get max of points1
+        if (commands1[a - 1][0] == 'L' || commands1[a - 1][0] == 'R') {
             if (maxw < points1[a].x) {
                 maxw = points1[a].x;
             }
@@ -139,8 +187,9 @@ function calculateAndDisplay(commands1, commands2) {
         }
 
     }
-    for (let a = 0; a < points2.length; a++) { // get max of points2
-        if (commands1[a][0] == 'L' || commands1[a][0] == 'R') {
+
+    for (let a = 1; a < points2.length; a++) { // get max of points2
+        if (commands2[a - 1][0] == 'L' || commands2[a - 1][0] == 'R') {
             if (maxw < points2[a].x) {
                 maxw = points2[a].x;
             }
@@ -181,16 +230,17 @@ function calculateAndDisplay(commands1, commands2) {
     var closestDist = 2147000000;
     var closestPoint;
     var startPoint = new point(map(0, minw, maxw, startx), map(0, minh, maxh, starty));
-    for (let a = 0; a < points1.length - 1; a++) {
-        for (let b = 0; b < points2.length - 1; b++) {
+
+    for (let b = 0; b < points2.length - 1; b++) {
+        for (let a = 0; a < points1.length - 1; a++) {
             var points1vector = new vector(points1[a], points1[a + 1]);
             var points2vector = new vector(points2[b], points2[b + 1]);
             if (intersect(points1vector, points2vector) != "NO") {
 
                 ctx.strokeStyle = "#FFFF00";
                 var iPoint = intersect(points1vector, points2vector);
-                console.log(distance(iPoint, new point(0, 0)));
-                if (closestDist > distance(iPoint, new point(0, 0)) && distance(iPoint, new point(0, 0)) != 0) {
+                // console.log(distance(iPoint, new point(0, 0)));
+                if (closestDist > distance(iPoint, new point(0, 0))) {
                     closestDist = distance(iPoint, new point(0, 0));
                     closestPoint = iPoint;
                 }
