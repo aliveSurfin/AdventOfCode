7d237738855cc93ffcaa87340b5c5cec85f5670d
alivesurfin <alivesurfin@gmail.com>
2021-12-01 05:29:35 +0000

    day1, day2 boiler

--- 2021/js/DAY01/day.js
+++ 2021/js/DAY01/day.js
@@ -2,6 +2,32 @@ let readfile = require('../utils/readfile');
 
 let data = readfile(true)
 
-data.forEach((e) => {
-    //console.log(e);
+//part1
+let countp1 = 0;
+data.forEach((e, i) => {
+    if (i >= 1) {
+        if (e > data[i - 1]) {
+            countp1++
+        }
+    }
 })
+
+//part 2
+let countp2 = 0;
+let cur = null;
+let prev;
+data.forEach((e, i) => {
+    if (i >= 2) {
+        prev = cur;
+        cur = 0;
+        for (let x = i;
+            (x >= 0) && (x > i - 3); x--) {
+            cur += data[x]
+        }
+        if (prev != null && cur > prev) {
+            countp2++;
+        }
+    }
+})
+console.log("part1: ", countp1);
+console.log("part2: ", countp2);
