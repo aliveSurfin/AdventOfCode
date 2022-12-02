7db01e0ed6ccba1b993b6be2a20026975717d0d6
Christy McCarron <alivesurfin@gmail.com>
2022-12-02 07:55:19 +0000

    Better with chars

--- 2022/kotlin/kotlin/src/main/kotlin/aoc/days/day02/day.kt
+++ 2022/kotlin/kotlin/src/main/kotlin/aoc/days/day02/day.kt
@@ -3,19 +3,23 @@ package aoc.days.day02
 import aoc.days.day0.DayBaseClass
 import aoc.utils.ReadInput
 
-class Day(testInput: Boolean = false) : DayBaseClass(testInput) {
-    // A = Rock, B = Paper, C = Scissors
-    // X = Rock, Y = Paper, Z = Scissors
-    private fun scores(a: String): Int? = mapOf("X" to 1, "Y" to 2, "Z" to 3)[a]
-    private fun winnerMap(a: String): Int? =
-        mapOf(
-            "AX" to 3, "AY" to 6, "AZ" to 0,
-            "BX" to 0, "BY" to 3, "BZ" to 6,
-            "CX" to 6, "CY" to 0, "CZ" to 3,
-        )[a]
+//Part 1: 12772
+//Part 2: 11618
+// A = Rock, B = Paper, C = Scissors
+// X = Rock, Y = Paper, Z = Scissors
 
-    private fun isWinner(lhs: String, rhs: String): Int {
-        return winnerMap(lhs + rhs)!!.plus(scores(rhs)!!)
+class Day(testInput: Boolean = false) : DayBaseClass(testInput) {
+
+    private fun scores(a: Char): Int = a.code - 87
+    private fun winnerMap(lhs: Char, rhs: Char): Int {
+        return when (rhs.code - lhs.code) {
+            23 -> 3
+            22, 25 -> 0
+            21, 24 -> 6
+            else -> {
+                throw NotImplementedError("")
+            }
+        }
     }
 
     @Override
@@ -24,11 +28,11 @@ class Day(testInput: Boolean = false) : DayBaseClass(testInput) {
     }
 
     fun part2() {
-        this.p2 = (this.input as List<List<String>>).map {
-            val result = if (it[1] == "X") 0 else if (it[1] == "Y") 3 else 6
-            val xResult = winnerMap(it[0] + "X")
-            val yResult = winnerMap(it[0] + "Y")
-            val zResult = winnerMap(it[0] + "Z")
+        this.p2 = (this.input as List<List<Char>>).sumOf {
+            val result = (it[1].code - 88) * 3
+            val xResult = winnerMap(it[0], 'X')
+            val yResult = winnerMap(it[0], 'Y')
+            val zResult = winnerMap(it[0], 'Z')
             when (result) {
                 xResult -> 1 + result
                 yResult -> 2 + result
@@ -37,18 +41,16 @@ class Day(testInput: Boolean = false) : DayBaseClass(testInput) {
                     throw NotImplementedError("X Y Z no result")
                 }
             }
-
-        }.sum()
+        }
     }
 
     fun part1() {
-
-        this.p1 = (this.input as List<List<String>>).sumOf { isWinner(it[0], it[1]) }
+        this.p1 = (this.input as List<List<Char>>).sumOf { winnerMap(it[0], it[1]).plus(scores(it[1])) }
     }
 
     @Override
     override fun solve() {
-        this.input = this.input.toString().replace("\r", "").split("\n").map { it.split(" ") }
+        this.input = this.input.toString().replace("\r", "").split("\n").map { it.split(" ").map { it.single() } }
         part1()
         part2()
     }
@@ -56,4 +58,6 @@ class Day(testInput: Boolean = false) : DayBaseClass(testInput) {
 
 fun main() {
     Day()
+
+
 }
