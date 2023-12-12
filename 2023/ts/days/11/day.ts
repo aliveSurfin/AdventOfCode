b5755aa1dac15293cd6ea439c26f37bc2c7c7797
alivesurfin <alivesurfin@gmail.com>
2023-12-12 00:34:27 +0000

    2023 11 - pathfinding slightly faster

--- 2023/ts/days/11/day.ts
+++ 2023/ts/days/11/day.ts
@@ -5,7 +5,6 @@ type Point = {
   x: number;
   y: number;
   parent: Point | null;
-  visited: boolean;
   char: string;
 };
 
@@ -15,7 +14,6 @@ class Day11 extends Day {
       x: point.x,
       y: point.y,
       parent: point.parent,
-      visited: point.visited,
       char: point.char,
     };
   }
@@ -29,67 +27,50 @@ class Day11 extends Day {
 
     this.grid.forEach((row, y) => {
       row.forEach((item, x) => {
-        // //console.log(item)
         if (item == "#") {
           cols[x] = true;
           rows[y] = true;
         }
       });
     });
-    //console.log("cols", cols);
-    //console.log("rows", rows);
     let added = 0;
     rows.forEach((hasPlanet, i) => {
-      // if (!hasPlanet) {
-      //   this.grid.splice(
-      //     i + added+1,
-      //     0,
-      //     new Array(this.grid[0].length).fill("*")
-      //   );
-      // }
       if (!hasPlanet) {
         this.grid[i] = new Array(this.grid[i].length).fill("*");
       }
     });
     added = 0;
-    //console.log("cols",cols)
     cols.forEach((hasPlanet, i) => {
-      // if (!hasPlanet) {
-      //   for (let y = 0; y < this.grid.length; y++) {
-      //     this.grid[y].splice(i + added+1, 0, "*");
-      //   }
-      //   added++;
-      // }
       if (!hasPlanet) {
         for (let y = 0; y < this.grid.length; y++) {
           this.grid[y][i] = "*";
         }
       }
     });
-    //console.log(this.grid.map((e) => e.join("")).join("\n"));
   }
 
   grid: string[][] = [];
 
-  safeAddToList(list: Point[], point: Point, deltaX: number, deltaY: number) {
+  safeAddToList(
+    point: Point,
+    deltaX: number,
+    deltaY: number
+  ): Point | null {
     let x = point.x + deltaX;
     let y = point.y + deltaY;
-    let newList = [...list];
     try {
       let char = this.grid[y][x];
       if (char == undefined) {
-        return newList;
+        return null;
       }
-      newList.push({
+      return {
         x: x,
         y: y,
-        parent: this.clone(point),
+        parent: point,
         char,
-        visited: false,
-      });
-      return newList;
+      };
     } catch {
-      return newList;
+      return null;
     }
   }
 
@@ -99,17 +80,14 @@ class Day11 extends Day {
     return dist;
   }
 
-  pathfind(planetA: Point, planetB: Point) {
-    let visited: Point[] = [];
-    let queue: Point[] = [];
-    let start = this.clone(planetA);
-    start.visited = true;
-    let end = this.clone(planetB);
-    queue.push(start);
+  pathfind(start: Point, end: Point) {
+    let closed: { [id: string]: Point } = {};
+    let open: Point[] = [];
+    open.push(start);
     let found: Point | null = null;
     let iterations = 0;
-    while (queue.length > 0) {
-      queue = queue.sort((a, b) => {
+    while (open.length > 0) {
+      open = open.sort((a, b) => {
         let manA = this.manhattan(a, end);
         let manB = this.manhattan(b, end);
 
@@ -123,8 +101,8 @@ class Day11 extends Day {
         return 0;
       });
       //@ts-ignore
-      let cur: Point = this.clone(queue.shift());
-      visited.push(this.clone(cur));
+      let cur: Point = open.shift();
+      closed[`${cur.x}-${cur.y}`] = cur;
       if (cur.x == end.x && cur.y == end.y) {
         found = cur;
         break;
@@ -137,11 +115,14 @@ class Day11 extends Day {
         [0, 1],
       ];
       directions.forEach((e) => {
-        adj = this.safeAddToList(adj, cur, e[0], e[1]);
+        let dirSafe = this.safeAddToList(cur, e[0], e[1]);
+        if (dirSafe != null) {
+          adj.push(dirSafe);
+        }
       });
       adj.forEach((e) => {
-        if (!visited.map((f) => `${f.x}-${f.y}`).includes(`${e.x}-${e.y}`)) {
-          queue.push(e);
+        if (closed[`${cur.x}-${cur.y}`] != null) {
+          open.push(e);
         }
       });
     }
@@ -153,44 +134,39 @@ class Day11 extends Day {
     this.grid.forEach((row, y) => {
       row.forEach((item, x) => {
         if (item == "#") {
-          planets.push({ x, y, parent: null, visited: false, char: "#" });
+          planets.push({ x, y, parent: null, char: "#" });
         }
       });
     });
     let dists: { [name: string]: number } = {};
     let dists2: { [name: string]: number } = {};
     planets.forEach((planetA, a) => {
-      
+      console.log(a)
       planets.forEach((planetB, b) => {
-        console.log(a,b)
+        let id = `${Math.min(a + 1, b + 1)} ${Math.max(a + 1, b + 1)}`
         if (
           a != b &&
-          dists[`${Math.min(a + 1, b + 1)} ${Math.max(a + 1, b + 1)}`] !== null
+          dists[id] !== null
         ) {
           let found = this.pathfind(planetA, planetB);
-          let path: Point[] = [];
-          while (found != null) {
-            path.push(found);
+          let expanded = 0
+          let other = 0
+          while (found!= null && found.parent != null) { // removing either side works
+            if(found.char=="*"){
+              expanded++
+            }else{
+              other ++
+            }
             found = found.parent;
           }
-          path = path.filter((e) => !(e.x == planetA.x && e.y == planetA.y));
-          dists[`${Math.min(a + 1, b + 1)} ${Math.max(a + 1, b + 1)}`] = path
-            .map((e) => {
-              return e.char == "*" ? 2 : 1;
-            })
-            .reduce(this.sum);
-          dists2[`${Math.min(a + 1, b + 1)} ${Math.max(a + 1, b + 1)}`] = path
-            .map((e) => {
-              return e.char == "*" ? 10 : 1;
-            })
-            .reduce(this.sum);
+          dists[id] = (expanded * 2) + other
+          dists2[id] = (expanded* 10) + other
         }
       });
     });
-
-
-    this.p1 = Object.values(dists).reduce(this.sum)
-    this.p2 = Object.values(dists2).reduce(this.sum)
+    console.log()
+    this.p1 = Object.values(dists).reduce(this.sum);
+    this.p2 = Object.values(dists2).reduce(this.sum);
   }
 
   override solveP2(): void {}
